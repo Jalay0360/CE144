08d1a6f31819acf5cea198a0cb8e7c9c7dfac096
Jalay0360 <114148983+Jalay0360@users.noreply.github.com>
2023-02-13 15:25:40 +0530

    Add files via upload

---
+++ 1_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"***************************************\n";
+    cout<<"*      Programming Assignment 1       *\n";
+    cout<<"*       Computer Programming 1        *\n";
+    cout<<"*  Author: Jalay Subhashbhai Movaliya *\n";
+    cout<<"*     Due Date: 29 February, 2023     *\n";
+    cout<<"***************************************\n";
+}
---
+++ 1_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1 ; i <= 4; i++)
+    {
+        for (int j = 1; j <= 4; j++)
+        {
+            cout<<setw(4)<<left<<i*j;
+        }
+        cout<<endl;
+    }
+}
